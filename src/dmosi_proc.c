b1718eaa0dc6fcf04d16c677c5a146e30f6a95a7
Patryk Kubiak <patryk.kubiak90@gmail.com>
2026-07-08 15:37:25 +0200

    Added support of context in process

--- src/dmosi_proc.c
+++ src/dmosi_proc.c
@@ -29,7 +29,7 @@ struct dmosi_process
 {
     uint64_t magic;                                 /**< Magic number for validation */
     char* name;                                     /**< Name of the process */
-    char* allocator_name;                           /**< Name+PID used to attribute heap allocations to this process instance */
+    Dmod_Context_t* context;                        /**< Dmod_Context_t this process is running, set via dmosi_process_set_context */
     dmosi_process_t parent;                         /**< Parent process (NULL for detached processes) */
     char module_name[DMOD_MAX_MODULE_NAME_LENGTH];  /**< Name of the associated module */
     int exit_status;                                /**< Exit status code (set when process is killed) */
@@ -222,18 +222,7 @@ DMOD_INPUT_API_DECLARATION( dmosi, 1.0, dmosi_process_t, _process_create,(const 
         return NULL;
     }
 
-    // Combine process name and PID so that concurrent instances of the same
-    // module get distinct heap allocation identities (see Dmod_GetCurrentAllocatorNameEx).
-    char allocator_name_buf[DMOD_MAX_MODULE_NAME_LENGTH + 16];
-    Dmod_SnPrintf(allocator_name_buf, sizeof(allocator_name_buf), "%s#%u", name, (unsigned)process->pid);
-    process->allocator_name = Dmod_StrDup(allocator_name_buf);
-    if(!process->allocator_name)
-    {
-        DMOD_LOG_ERROR("Failed to build allocator name for process %s of module %s\n", name, module_name);
-        Dmod_Free(process->name);
-        Dmod_Free(process);
-        return NULL;
-    }
+    process->context = NULL; /* set via dmosi_process_set_context once the module to run is known */
     process->parent = parent;
     strcpy(process->module_name, module_name);
 
@@ -283,7 +272,6 @@ DMOD_INPUT_API_DECLARATION( dmosi, 1.0, void, _process_destroy, (dmosi_process_t
     }
 
     Dmod_Free(process->name);
-    Dmod_Free(process->allocator_name);
     Dmod_Free(process->pwd);
     Dmod_Free(process);
 
@@ -410,14 +398,25 @@ DMOD_INPUT_API_DECLARATION( dmosi, 1.0, const char*, _process_get_module_name, (
     return process->module_name;
 }
 
-DMOD_INPUT_API_DECLARATION( dmosi, 1.0, const char*, _process_get_allocator_name, (dmosi_process_t process) )
+DMOD_INPUT_API_DECLARATION( dmosi, 1.0, int, _process_set_context, (dmosi_process_t process, Dmod_Context_t* context) )
+{
+    if(!validate_process(process))
+    {
+        DMOD_LOG_ERROR("Invalid process handle provided to set context\n");
+        return -EINVAL;
+    }
+    process->context = context;
+    return 0;
+}
+
+DMOD_INPUT_API_DECLARATION( dmosi, 1.0, Dmod_Context_t*, _process_get_context, (dmosi_process_t process) )
 {
     if(!process)
     {
-        DMOD_LOG_ERROR("Cannot get allocator name of NULL process\n");
+        DMOD_LOG_ERROR("Cannot get context of NULL process\n");
         return NULL;
     }
-    return process->allocator_name;
+    return process->context;
 }
 
 DMOD_INPUT_API_DECLARATION( dmosi, 1.0, int,            _process_set_uid,   (dmosi_process_t process, dmosi_user_id_t uid) )
